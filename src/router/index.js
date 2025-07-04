8de789c42dbdd3b9b6faa326aebd8e5a1c07300e
rohith10e <rohith10e@outlook.com>
2025-07-04 21:43:55 +0530

    feat: Edit post

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,9 @@ import JobsView from "@/views/JobsView.vue";
 import NoRouteView from "@/views/NoRouteView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
-const route=[{path:'/',name:'home',component:HomeView},{path:'/jobs',name:'jobs',component:JobsView},{path:'/:pathMatch(.*)',component:NoRouteView},{path:'/jobs/:id',name:'job',component:JobView,props:true},{path:'/jobs/add',name:'add job',component:AddJobView}]
+const route=[{path:'/',name:'home',component:HomeView},{path:'/jobs',name:'jobs',component:JobsView},{path:'/:pathMatch(.*)',component:NoRouteView},{path:'/jobs/:id',name:'job',component:JobView,props:true},{path:'/jobs/add',name:'add job',component:AddJobView},{path:'/edit/:id',name:'edit job',component:EditJobView}]
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes:route
